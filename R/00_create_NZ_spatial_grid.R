49ce2c82fde3400073195d92c87dca276fc007ac
garciacallejas <david.garcia.callejas@gmail.com>
2021-12-29 16:29:39 +0100

    create spatial grid; script for gathering observations from GBIF

---
+++ R/00_create_NZ_spatial_grid.R
@@ -0,0 +1,36 @@
+
+
+# create 1km grid over a NZ shapefile
+library(sf)
+library(tidyverse)
+
+# this should be already in WGS84
+NZ <- st_read('../datasets/spatial_data/NZ_main_islands.shp')
+
+# -------------------------------------------------------------------------
+
+# create 1km grid - in these units, 0.1 is 1km
+# TODO check - that is not right, it should be .0001 I think
+grid_1 <- st_make_grid(NZ, cellsize = c(1, 1)) %>% 
+  st_sf(grid_id = 1:length(.))
+
+# crop grid, keep only cells with land
+grid_1_land <- st_intersection(grid_1,NZ)
+
+# create labels for each grid_id
+grid_lab <- st_centroid(grid_1_land) %>% cbind(st_coordinates(.))
+
+# view the polygons and grid
+# ggplot() +
+#   # geom_sf(data = NZ, fill = 'white', lwd = 0.05) +
+#   # geom_sf(data = pts, color = 'red', size = 1.7) +
+#   geom_sf(data = grid_1_land, fill = 'transparent', lwd = 0.3) +
+#   # geom_text(data = grid_lab, aes(x = X, y = Y, label = grid_id), size = 2) +
+#   coord_sf(datum = NA)  +
+#   labs(x = "") +
+#   labs(y = "")
+
+# -------------------------------------------------------------------------
+
+st_write(grid_lab,"data/NZ_grid_labs.csv")
+st_write(grid_1_land,"data/NZ_grid.shp")
---
+++ R/00_get_sp_observations.R
@@ -0,0 +1,171 @@
+library(rgbif)
+library(tidyverse)
+library("rnaturalearth")
+library(sf)
+library(ggspatial)
+library(countrycode)
+library(CoordinateCleaner)
+
+# sources:
+# https://rpubs.com/julianlavila/GbifCleanning
+
+# -------------------------------------------------------------------------
+# NZ grid
+NZ_grid <- st_read("data/NZ_grid.shp")
+
+# list of interactions with names of taxa
+int.list <- read.csv("../datasets/plant-bird interactions and traits/plant_bird_interactions.csv")
+sp.list <- unique(c(sp.list$PLANTSPECIES,sp.list$BIRDSPECIES))
+
+sp.list <- str_replace(sp.list,"_"," ")
+# TODO: remove "sp" from taxa at the genus level
+
+# -------------------------------------------------------------------------
+# gather observations from every species
+for(i.sp in 1:length(sp.list)){
+  
+  #obtain data from GBIF via rgbif
+  dat <- occ_search(scientificName = sp.list[i.sp], 
+                    limit = 500, # hard limit is 100k
+                    country = "NZ", 
+                    hasCoordinate = T)
+  
+  # names(dat$data) #a lot of columns
+  if(!is.null(dat$data)){
+    
+    #select columns of interest
+    dat <- dat$data %>%
+      dplyr::select(species, decimalLongitude, decimalLatitude, countryCode,
+                    gbifID,
+                    coordinateUncertaintyInMeters, year,
+                    basisOfRecord, institutionCode, datasetName)
+    # names(dat)
+    
+    # clean up records without coordinates
+    dat <- dat %>%
+      filter(!is.na(decimalLongitude))%>%
+      filter(!is.na(decimalLatitude))
+    
+    #plot data to get an overview
+    # wm <- borders("world", colour="gray70", fill="gray80")
+    # ggplot()+ 
+    #   coord_fixed()+ 
+    #   wm +
+    #   geom_point(data = dat, 
+    #              aes(x = decimalLongitude, y = decimalLatitude),
+    #              colour = "red", size = 0.9)+
+    #   theme_bw()+ 
+    #   labs(title = paste("Spatial distribution of records",dat$species))
+    
+    # more clean up of records with potential errors in different fields
+    
+    #convert country code from ISO2c to ISO3c
+    dat$countryCode <-  countrycode(dat$countryCode, 
+                                    origin =  'iso2c', 
+                                    destination = 'iso3c')
+    
+    #flag problems
+    dat <- data.frame(dat)
+    flags <- clean_coordinates(x = dat, 
+                               lon = "decimalLongitude", lat = "decimalLatitude",
+                               countries = "countryCode", 
+                               species = "species",
+                               tests = c("capitals", "centroids", "equal",
+                                         "gbif", "institutions",
+                                         "zeros")) # most test are on by default
+    
+    # summary(flags)
+    
+    # exclude problematic records
+    dat_cl <- dat[flags$.summary,]
+    
+    # uncertainty of spatial coordinates
+    hist(dat_cl$coordinateUncertaintyInMeters / 1000, breaks = 20)
+    
+    # remove uncertain records
+    dat_cl <- dat_cl %>% 
+      dplyr::filter(coordinateUncertaintyInMeters / 1000 <= 100 | is.na(coordinateUncertaintyInMeters))
+    
+    # remove suspicious data sources - in this case all are ok
+    # table(dat_cl$basisOfRecord)
+    # dat_cl <- filter(dat_cl, basisOfRecord == "HUMAN_OBSERVATION")
+    # dat_cl$year <- as.factor(dat_cl$year)
+    # dat_cl %>% 
+    #   group_by(year) %>% 
+    #   summarise(records=n()) %>% 
+    #   ggplot(aes(year, records, group = 1))+
+    #   geom_line(color="steelblue")+
+    #   labs(title=paste("Number of records of:",dat_cl$species, "by year"),
+    #        y="",x="")+
+    #   theme_bw()
+    
+    # -------------------------------------------------------------------------
+    # assign a grid_id to each observation within the limits
+    
+    projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+    obs <- st_as_sf(x = dat_cl,                         
+                    coords = c("decimalLongitude", "decimalLatitude"),
+                    crs = projcrs)
+    observations_id <- st_intersection(obs,NZ_grid)
+    
+    # convert to standard dataframe
+    obs_id_df <- observations_id[,c("grid_id","species","year","geometry","gbifID")] %>%
+      mutate(lat = sf::st_coordinates(.)[,2],
+             lon = sf::st_coordinates(.)[,1]) %>%
+      sf::st_set_geometry(NULL)
+    
+    # -------------------------------------------------------------------------
+    # write to disk
+    
+    write.csv2(obs_id_df,paste("results/sp_observations/",
+                               sp.list[i.sp],
+                               ".csv",sep=""),row.names = FALSE)
+    
+  }# if !is.null
+}# for i.sp
+
+# -------------------------------------------------------------------------
+# plot test
+
+# ggplot() +
+#   geom_sf(NZ_grid, fill = 'transparent', lwd = 0.3, mapping = aes()) +
+#   # geom_text(data = grid_lab, aes(x = X, y = Y, label = grid_id), size = 3, color = "darkred") +
+#   # coord_sf(datum = NA)  +
+#   geom_sf(data = observations_id,
+#              colour = "cyan4", size = 1.2, alpha=0.6)+
+#   NULL
+
+# -------------------------------------------------------------------------
+# another way of plotting the data
+
+# world <- ne_countries(scale = "medium", returnclass = "sf")
+# 
+# coords<-dat_cl %>% 
+#   dplyr::select(species,decimalLongitude, decimalLatitude ) %>%
+#   rename(Longitude=decimalLongitude, Latitude=decimalLatitude)
+# 
+# coords_sf<- coords %>% 
+#   st_as_sf(coords=(2:3))
+# 
+# bbox_new <- st_bbox(coords_sf)
+# 
+# ggplot()+
+#   geom_sf(data = world, fill= NA) +
+#   coord_sf(xlim=c(bbox_new[1],bbox_new[3]), 
+#            ylim=c(bbox_new[2],bbox_new[4]))+
+#   geom_point(data = dat_cl, aes(x = decimalLongitude, y = decimalLatitude),
+#              colour = "cyan4", size = 1.2, alpha=0.6)+
+#   annotation_scale(location = "br", width_hint = 0.1,line_width = 0.5) +
+#   annotation_north_arrow(location = "tr", which_north = "true", 
+#                          pad_x = unit(1.5, "cm"), 
+#                          pad_y = unit(0.5, "cm"),
+#                          height = unit(1, "cm"),
+#                          width = unit(1, "cm"), # 0.2 # 0.3
+#                          style = north_arrow_fancy_orienteering)+
+#   labs(x = "", y = "",
+#        title =paste("Record distribution of:", dat_cl$species),
+#        caption = paste("Source: GBif\nAuthor: yep\nDate:",
+#                        format(Sys.time(), '%d %B, %Y')))+
+#   theme(plot.title=element_text(size=19),
+#         plot.caption = element_text(size = 7, color="grey60"),
+#         panel.background= element_rect(fill = "grey96"))
