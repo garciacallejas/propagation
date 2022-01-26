4b892c8b13c28c3c3dfb7cb96aa985b67091d036
garciacallejas <david.garcia.callejas@gmail.com>
2022-01-26 19:23:58 +0100

    work in progress: communicability calculation

--- R/01_get_communicability.R
+++ R/01_get_communicability.R
@@ -1,13 +1,14 @@
 
 # communicability_ij = exp(A)ij
 
+library(data.table)
 # library(tidyverse)
 library(expm) # for matrix exponentiation
 # library(igraph)
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
 # -------------------------------------------------------------------------
-load("results/community_block_matrix.Rdata")
+# load("results/community_block_matrix.Rdata")
 
 # these are for building the list of interactions
 clean.int.data <- read.csv2("data/plant_bird_clean_interaction_data.csv")
@@ -27,32 +28,41 @@ represented.cells <- as.numeric(substr(represented.cells[-1],
 
 # is my matrix binary?
 is.binary <- sum(block.matrix > 1) == 0
-if(is.binary){
-  # binary
-  # beware, it takes at least > 20 min for a 200M elements matrix
-  binary.comm.matrix <- expm(block.matrix)
-  weighted.comm.matrix <- 0
-  
-}else{
-  # binary and weighted
-  binary.matrix <- ifelse(block.matrix != 0, 1, 0)
-  binary.comm.matrix <- expm(binary.matrix)
-  # TODO consider weighting these elements by community size
-  
-  # weighted
-  abs.int.matrix <- abs(block.matrix)
-  degree.diag.matrix <- matrix(0,nrow = nrow(binary.matrix),
-                               ncol = ncol(binary.matrix))
-  diag(degree.diag.matrix) <- rowSums(abs.int.matrix) # strength - suma de los pesos
-  
-  d.inverse <- solve(degree.diag.matrix)
-  d.pow <- sqrt(d.inverse)
-  
-  multiplied.matrix <- d.pow %*% abs.int.matrix %*% d.pow # esto es eq. 2.2 de Crofts and Higham 2009
-  weighted.comm.matrix <- expm(multiplied.matrix)
-}
+# if(is.binary){
+#   # binary
+#   # beware, it takes at least > 20 min for a 200M elements matrix
+#   binary.comm.matrix <- expm(block.matrix)
+#   weighted.comm.matrix <- 0
+#   
+# }else{
+#   # binary and weighted
+#   binary.matrix <- ifelse(block.matrix != 0, 1, 0)
+#   binary.comm.matrix <- expm(binary.matrix)
+#   # TODO consider weighting these elements by community size
+#   
+#   # weighted
+#   abs.int.matrix <- abs(block.matrix)
+#   degree.diag.matrix <- matrix(0,nrow = nrow(binary.matrix),
+#                                ncol = ncol(binary.matrix))
+#   diag(degree.diag.matrix) <- rowSums(abs.int.matrix) # strength - suma de los pesos
+#   
+#   d.inverse <- solve(degree.diag.matrix)
+#   d.pow <- sqrt(d.inverse)
+#   
+#   multiplied.matrix <- d.pow %*% abs.int.matrix %*% d.pow # esto es eq. 2.2 de Crofts and Higham 2009
+#   weighted.comm.matrix <- expm(multiplied.matrix)
+# }
+# 
+# save(binary.comm.matrix,file = "results/binary_communicability_matrix.Rdata")
+# save(weighted.comm.matrix,file = "results/weighted_communicability_matrix.Rdata")
+# 
+# rm(block.matrix)
+# gc(verbose = F)
 
 # -------------------------------------------------------------------------
+
+load("results/binary_communicability_matrix.Rdata")
+load("results/weighted_communicability_matrix.Rdata")
 
 if(is.binary){
   df1 <- expand.grid(sp1 = 1:length(all.sp),
@@ -60,22 +70,27 @@ if(is.binary){
                      sp2 = 1:length(all.sp),
                      grid.id.sp2 = represented.cells,
                      binary.communicability = 0)
+  df1 <- as.data.table(df1)
+  
+  # TODO consider other options to make it faster, e.g. renaming matrix
+  # dimensions as sp_grid (1_1, etc), and do an automatic conversion to dataframe
   
   for(i.pos in 1:nrow(df1)){
     
-    my.sp1 <- which(all.sp == df1$sp1[i.pos])
-    my.grid.id.sp1 <- df1$grid.id.sp1[i.pos]
-    my.sp2 <- df1$sp2[i.pos]
-    my.grid.id.sp2 <- df1$grid.id.sp2[i.pos]
+    # my.sp1 <- which(all.sp == df1$sp1[i.pos])
+    # my.grid.id.sp1 <- df1$grid.id.sp1[i.pos]
+    # my.sp2 <- which(all.sp == df1$sp2[i.pos])
+    # my.grid.id.sp2 <- df1$grid.id.sp2[i.pos]
     
-    my.matrix.row <-  num.sp * (my.grid.id.sp1 - 1) + my.sp.num
+    my.matrix.row <- num.sp * (df1$grid.id.sp1[i.pos] - 1) + df1$sp1[i.pos]
+    my.matrix.col <- num.sp * (df1$grid.id.sp2[i.pos] - 1) + df1$sp2[i.pos]
     
-    df1$binary.communicability[i.pos] <- binary.comm.matrix[df1$x[i.pos],df1$y[i.pos]]
+    df1$binary.communicability[i.pos] <- binary.comm.matrix[my.matrix.row,my.matrix.col]
     # df1$weighted.communicability[i.pos] <- weighted.comm.matrix[df1$x[i.pos],df1$y[i.pos]]
   }
   
   df1$scaled.binary.communicability <- range01(df1$binary.communicability)
-  df1$diag <- ifelse(df1$x == df1$y,TRUE,FALSE)
+  df1$diag <- ifelse(df1$sp1 == df1$sp2,TRUE,FALSE)
 
 }else{
   df1 <- expand.grid(sp1 = 1:length(all.sp),
@@ -86,7 +101,15 @@ if(is.binary){
                      weighted.communicability = 0)
   
   for(i.pos in 1:nrow(df1)){
-    df1$binary.communicability[i.pos] <- binary.comm.matrix[df1$x[i.pos],df1$y[i.pos]]
+    my.sp1 <- which(all.sp == df1$sp1[i.pos])
+    my.grid.id.sp1 <- df1$grid.id.sp1[i.pos]
+    my.sp2 <- which(all.sp == df1$sp2[i.pos])
+    my.grid.id.sp2 <- df1$grid.id.sp2[i.pos]
+    
+    my.matrix.row <- num.sp * (my.grid.id.sp1 - 1) + my.sp1
+    my.matrix.col <- num.sp * (my.grid.id.sp2 - 1) + my.sp2
+    
+    df1$binary.communicability[i.pos] <- binary.comm.matrix[my.matrix.row,my.matrix.col]
     df1$weighted.communicability[i.pos] <- weighted.comm.matrix[df1$x[i.pos],df1$y[i.pos]]
   }
   
@@ -96,7 +119,7 @@ if(is.binary){
 
 # -------------------------------------------------------------------------
 
-write.csv2(df1,"results/communicability_pairwise.csv",row.names = FALSE)
+write_csv2(df1,"results/communicability_pairwise.csv",row.names = FALSE)
 
 # -------------------------------------------------------------------------
 # load("data/interaction_matrix.Rdata")
